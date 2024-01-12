932b38398bdbd87dc426fa4f598633f476e43b76
zahid-800 <154730312+zahid-800@users.noreply.github.com>
2024-01-12 22:59:46 +0600

    Add files via upload

---
+++ Javascript/main.js
@@ -0,0 +1,18 @@
+function search(){
+    let fillter=document.getElementById('find').Value.toUpperCase();
+    let item=document.querySelectorAll('.product');
+    let x=document.getElementsByTagName('h3');
+    
+
+    for(var i =0; i<=i. length; i++){
+          let a= item [i].getElementsByTagName('h3') [0];
+          let value= a.innerHTML ||  a.innerText  || a.textContent ;
+
+          if(value.toUpperCase().indexOf(fillter) > -1){
+            item [i].style.display=" ";
+          }
+        else{
+            item [i].style.display="none";
+          }  
+    }
+}
